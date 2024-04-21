f0b9f571396a4f4fd9dbc44b1fce557f67edd27c
appsecandrewlee <appsec.andrewlee@gmail.com>
2024-04-21 15:04:24 +1000

    correct boardlayout printed, setting everything to 0

--- Application.cpp
+++ Application.cpp
@@ -6,7 +6,7 @@ int main() {
 
   BitBoard bitboard;
 
-  unsigned long long BoardLayout;
+  unsigned long long BoardLayout = 0;
   bitboard.BOARD_SETUP(BoardLayout);
 
   return 0;
--- Board/BitBoard.h
+++ Board/BitBoard.h
@@ -2,19 +2,25 @@
 #include <iostream>
 /* Author: Andrew Lee
  * This header file is used to initialise the board for Chess
- *
+ * SOLID Principles
  *
  */
 class BitBoard {
+
 public:
   void BOARD_SETUP(unsigned long long BitBoard) {
 
     for (int rank = 0; rank < 8; rank++) {   // creating 8 rows
       for (int file = 0; file < 8; file++) { // creating 8 columns
         int square = rank * 8 + file; // each tile, that calculates 0 - 63
-        printf("%d", square);         // print out the tiles
+
+        if (BitBoard & (1ULL << square)) {
+          printf("1");
+        } else {
+          printf("0");
+        }
       }
-      printf("\n"); // Layout 8 by 8
+      printf("\n");
     }
   }
 };
